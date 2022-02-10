fa632b7a2efcb2b9e81b76b27b50cc84f4e49779
Kari Ikonen <mr.kari.ikonen@gmail.com>
2022-02-11 00:16:46 +0200

    FIX permissions for volumes FIX server.pid in tmpfs to avoid stale PID FIX incorrect assets volume path FIX user UID/GID setting properly

--- host-service/docker/docker-entrypoint.sh
+++ host-service/docker/docker-entrypoint.sh
@@ -3,19 +3,14 @@
 #socat TCP-LISTEN:11211,fork TCP:memcached:11211 &
 #socat TCP-LISTEN:6379,fork TCP:redis:6379 &
 
-sudo chown -R docker:users /bundle /node_modules /app/log /app/tmp /app/public
-
 echo "SERVER_MODE: $SERVER_MODE"
 
 if [[ "$SERVER_MODE" == "debug" ]]; then
     sleep infinity
 else
     bundle check || bundle install
-
-    # NOTE KI *NEW* intance clear old trash
-    if [[ -f /app/tmp/pids/server.pid ]]; then
-        rm /app/tmp/pids/server.pid
-    fi
-
     bundle exec rails s -b 0.0.0.0 -p 3000
 fi
+
+# TODO KI https://ieftimov.com/post/docker-compose-stray-pids-rails-beyond/
+#exec "$@"
