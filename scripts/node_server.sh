1032353643f3d586771d3ff5ee168f697affd31d
Kari Ikonen <mr.kari.ikonen@gmail.com>
2022-03-25 23:14:14 +0200

    FIX param check

--- scripts/node_server.sh
+++ scripts/node_server.sh
@@ -3,8 +3,14 @@
 . $(realpath $(dirname $0))/base_env.sh
 
 CONTAINER=$1
+
+if [[ $CONTAINER == "" ]]; then
+    echo "USAGE: $0 [CONTAINER]"
+    exit
+fi
+
 SERVICE_MODE=debug $DOCKER_COMPOSE up -d $CONTAINER
 
 shift
 $DOCKER_COMPOSE exec $CONTAINER bash -c "yarn install"
-$DOCKER_COMPOSE exec $CONTAINER yarn run start
+$DOCKER_COMPOSE exec $CONTAINER bash -c "yarn run start"
