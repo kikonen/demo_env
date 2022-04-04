580b5cc5ce5c3d1a763eee9c3984d53d2a063633
Kari Ikonen <mr.kari.ikonen@gmail.com>
2022-04-04 19:22:07 +0300

    FIX skip trash

--- scripts/setup_repositories.sh
+++ scripts/setup_repositories.sh
@@ -8,10 +8,14 @@ REPOSITORIES=$(ls $PROJECTS_DIR)
 echo "SETUP REPOSITORIES: $REPOSITORIES"
 
 echo "$REPOSITORIES" | tr ' ' '\n' | while read REPO; do
+    if [[ $REPO == "" ]]; then
+        continue
+    fi
+
     REPO_DIR="${PROJECTS_DIR}/${REPO}"
     SERVICE_DIR="${ROOT_DIR}/${REPO}-service"
 
-    if [[ ! -d $REPO_DIR ]]; then
+    if [[ -d $REPO_DIR ]]; then
         echo "N/A: $REPO_DIR"
         continue
     fi
